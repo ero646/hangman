2dd7c52b60a72ff49f04cca9970ea7dd99e48019
ero646 <enriquerojas91@gmail.com>
2017-11-09 19:27:27 -0600

    Add files via upload

---
+++ game.js
@@ -0,0 +1,5 @@
+//list of words to guess.
+exports.newWord = {
+  wordList:['SUPERMAN','BATMAN','WONDER WOMAN','THE FLASH','IRON MAN','SPIDER MAN','CAPTAIN AMERICA','THOR','NIGHTWING','DR STRANGE','HULK','GREEN LANTERN','SUPERGIRL'],
+  hangman: ["\n","\n","\n","\n","\n","\n","\n","\n","\n","\n",]
+};
